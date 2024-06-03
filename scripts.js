b07430edb220d3a0f5006ef3af7d24bde31c685b
IndukumarPerla <146165012+IndukumarPerla@users.noreply.github.com>
2024-06-03 14:12:08 +0530

    updated

--- scripts.js
+++ scripts.js
@@ -6,7 +6,27 @@ document.addEventListener('DOMContentLoaded', function () {
     menu.classList.toggle('active');
   });
 });
+document.addEventListener('DOMContentLoaded', function() {
+  var dropdown = document.getElementById('school-dropdown');
+  var menu = document.getElementById('school-menu');
 
+  dropdown.addEventListener('click', function() {
+    if (menu.style.display === 'block' || menu.style.display === '') {
+      menu.style.display = 'none';
+    } else {
+      menu.style.display = 'block';
+    }
+  });
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.addEventListener('click', function(event) {
+    if (!event.target.matches('#school-dropdown')) {
+      if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+      }
+    }
+  });
+});
 // Smooth scroll functionality for the scroll-down arrow
 document.querySelector('.scroll-down').addEventListener('click', function(e) {
   e.preventDefault();
